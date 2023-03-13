8eb6ad1580ede693fec5b81f73f8b6481fe15e2b
Milan Trbojevic <mtrbojevic12@gmail.com>
2023-03-13 20:08:07 +0100

    working on category form setup categories selector

---
+++ src/app/categories/components/categories-data-table/categories-data-table.component.ts
@@ -0,0 +1,74 @@
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Category} from '../../core/models/category';
+import {Router} from '@angular/router';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {Table} from 'primeng/table';
+
+@Component({
+    selector: 'categories-data-table',
+    templateUrl: './categories-data-table.component.html',
+    styleUrls: ['./categories-data-table.component.scss']
+})
+export class CategoriesDataTableComponent {
+    @Input() data: Category[] = [];
+
+    @ViewChild('filter') filter!: ElementRef;
+
+    constructor(private confirmationService: ConfirmationService,
+                private messageService: MessageService,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    /**
+     * Method for handling
+     * global filter across data.
+     *
+     * @param table table identifier
+     * @param event filter event type
+     */
+    onGlobalFilter(table: Table, event: Event) {
+        table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+    }
+
+    /**
+     * Method for clearing
+     * global filter input data.
+     *
+     * @param table
+     */
+    clear(table: Table) {
+        table.clear();
+        this.filter.nativeElement.value = '';
+    }
+
+    /**
+     * Redirect user to news
+     * edit page.
+     *
+     * @param id id of selected news item.
+     */
+    goToEditPage = (id: string) => this.router.navigateByUrl('/admin/categories/edit/' + id).then();
+
+    /**
+     * Redirect user to news
+     * create page.
+     *
+     */
+    goToCreatePage = () => this.router.navigateByUrl('/admin/categories/create').then();
+
+    /**
+     * Trigger popup to
+     * confirm deleting selected
+     * news item from data table.
+     */
+    confirmDelete(): void {
+        this.confirmationService.confirm({
+            key: 'confirmDeleteDialog'
+        });
+    }
+}
+
