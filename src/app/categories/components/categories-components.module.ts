bf31bbe6ecc2f4bcaf3fff8d5c4fd381837f95b3
Milan Trbojevic <riverDeer12@gmail.com>
2023-03-14 02:00:53 +0100

    design changes

--- src/app/categories/components/categories-components.module.ts
+++ src/app/categories/components/categories-components.module.ts
@@ -7,13 +7,13 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {HttpClient} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import { CategoriesDataTableComponent } from './categories-data-table/categories-data-table.component';
+import {CategoriesDataTableComponent} from './categories-data-table/categories-data-table.component';
 import {TableModule} from 'primeng/table';
 import {RippleModule} from 'primeng/ripple';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
 import {ConfirmationService} from 'primeng/api';
 import {MultiSelectModule} from 'primeng/multiselect';
-import { CategoriesSelectorComponent } from './categories-selector/categories-selector.component';
+import {CategoriesSelectorComponent} from './categories-selector/categories-selector.component';
 
 /**
  * Translation resources loader.
