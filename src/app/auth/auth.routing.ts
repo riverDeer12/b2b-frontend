9eca33ec8d024eef1382cb8a27d38ce0f2e005cc
riverDeer12 <mtrbojevic12@gmail.com>
2024-03-30 23:27:27 +0100

    moved email confirmed to public layout pages, because of api redirect

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -5,7 +5,7 @@ import {ForbiddenComponent} from './pages/forbidden/forbidden.component';
 import {ErrorComponent} from './pages/error/error.component';
 import {DefaultGuard} from '../shared/guards/default.guard';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
-import {MailConfirmedComponent} from "./pages/mail-confirmed/mail-confirmed.component";
+import {MailConfirmedComponent} from "../layout/public/pages/mail-confirmed/mail-confirmed.component";
 
 export const AuthRoutes: Routes = [
     {
--- src/app/auth/pages/auth-pages.module.ts
+++ src/app/auth/pages/auth-pages.module.ts
@@ -16,7 +16,6 @@ import {ForbiddenComponent} from './forbidden/forbidden.component';
 import {ErrorComponent} from './error/error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import {RadioButtonModule} from 'primeng/radiobutton';
-import { MailConfirmedComponent } from './mail-confirmed/mail-confirmed.component';
 
 @NgModule({
     declarations: [
@@ -25,7 +24,6 @@ import { MailConfirmedComponent } from './mail-confirmed/mail-confirmed.componen
         ForbiddenComponent,
         ErrorComponent,
         NotFoundComponent,
-        MailConfirmedComponent
     ],
     imports: [
         CommonModule,
