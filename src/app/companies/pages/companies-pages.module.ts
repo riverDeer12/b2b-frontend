9b207213662991d9024bed5b7e12e91afa24c343
Milan Trbojevic <mtrbojevic12@gmail.com>
2024-05-29 13:31:49 +0200

    #165 - refactoring

--- src/app/companies/pages/companies-pages.module.ts
+++ src/app/companies/pages/companies-pages.module.ts
@@ -1,14 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CompaniesHomeComponent} from './companies-home/companies-home.component';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HttpClient} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateModule} from '@ngx-translate/core';
 import {CompanyCreateComponent} from './company-create/company-create.component';
 import {CompanyEditComponent} from './company-edit/company-edit.component';
 import {CompaniesComponentsModule} from '../components/companies-components.module';
-import {ConfirmationService} from 'primeng/api';
-
 
 @NgModule({
     declarations: [
