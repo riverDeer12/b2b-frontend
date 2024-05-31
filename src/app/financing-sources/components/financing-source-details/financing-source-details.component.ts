159710909f665c819c4283677b8a170cc2070414
riverDeer12 <mtrbojevic12@gmail.com>
2024-05-31 18:21:31 +0200

    #165 - refactoring

--- src/app/financing-sources/components/financing-source-details/financing-source-details.component.ts
+++ src/app/financing-sources/components/financing-source-details/financing-source-details.component.ts
@@ -1,7 +1,5 @@
 import {Component, Input} from '@angular/core';
 import {FinancingSource} from "../../core/models/financing-source";
-import {Category} from "../../../categories/core/models/category";
-import {ActivatedRoute} from "@angular/router";
 
 @Component({
     selector: 'financing-source-details',
